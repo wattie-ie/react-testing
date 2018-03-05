a4df5eecc40b626425243bf51be7b02f67e11697
Jason <jason@Jasons-MBP.local>
2018-03-05 14:25:41 +1100

    Added Add and Remove functionality.

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -6,17 +6,23 @@ class HelloWorld extends Component {
     super(props);
     this.state = { greeting: 'Hello' };
     this.frenchify = this.frenchify.bind(this);
+    this.removeGreeting = this.removeGreeting.bind(this);
   }
   render() {
     return (
       <div className="HelloWorld">{this.state.greeting} {this.props.name}!
       <br />
       <button onClick={this.frenchify}>Frenchify!</button>
+      <br/>
+      <button onClick={this.removeGreeting}>Remove Me!</button>
       </div>
     );
   }
   frenchify() {
     this.setState({ greeting: 'Bonjour' });
+  }
+  removeGreeting() {
+    this.props.removeGreeting(this.props.name);
   }
 }
 
--- src/HelloWorldList.js
+++ src/HelloWorldList.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
 import './HelloWorldList.css';
 import HelloWorld from './HelloWorld';
+import AddGreeter from './AddGreeter';
 
 class HelloWorldList extends Component {
     constructor(props) {
         super(props);
         this.state = { greetings: ['Jim', 'Sally','Bender'] };
+        this.addGreeting = this.addGreeting.bind(this);
+        this.removeGreeting = this.removeGreeting.bind(this);
     }
     render() {
       return (
         <div className="HelloWorldList">
+            <AddGreeter addGreeting={this.addGreeting}/>
             {this.renderGreetings()}
         </div>
       );
     }
     renderGreetings() {
         return this.state.greetings.map(name => (
-          <HelloWorld key={name} name={name}/>
+          <HelloWorld
+            key={name}
+            name={name}
+            removeGreeting={this.removeGreeting}
+          />
         ));
+    }
+    addGreeting(newName) {
+      this.setState({ greetings: [...this.state.greetings, newName] });
+    }
+    removeGreeting(removeName) {
+      const filteredGreetings = this.state.greetings.filter(name => {
+        return name !== removeName;
+      });
+      this.setState({ greetings: filteredGreetings });
     }
   }
   export default HelloWorldList;
